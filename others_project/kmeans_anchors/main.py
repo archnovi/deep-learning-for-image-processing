2135672b87d8c4d03020846c22d86b478dfe8057
archnovi <164203314+archnovi@users.noreply.github.com>
2024-12-18 19:34:00 +0800

    Update main.py

--- others_project/kmeans_anchors/main.py
+++ others_project/kmeans_anchors/main.py
@@ -8,12 +8,12 @@ from yolo_kmeans import k_means, wh_iou
 
 
 def anchor_fitness(k: np.ndarray, wh: np.ndarray, thr: float):  # mutation fitness
-    r = wh[:, None] / k[None]
-    x = np.minimum(r, 1. / r).min(2)  # ratio metric
+    r = wh[:, None] / k[None] # 如果聚类k和标注框重合度更高，r越接近1。其他的会比1小也可能比1大
+    x = np.minimum(r, 1. / r).min(2)  # ratio metric   np.minimum(r, 1. / r) 将比1大的情况都转为比1小。 min求（高度与高度的ratio，宽度与宽度的ratio）两个数的最小值（匹配最差的边）
     # x = wh_iou(wh, k)  # iou metric
-    best = x.max(1)
-    f = (best * (best > thr).astype(np.float32)).mean()  # fitness
-    bpr = (best > thr).astype(np.float32).mean()  # best possible recall
+    best = x.max(1) # best得到的是每个标注框与其匹配度最高的k（anchor）的匹配最差的边   max(1)表示第二个维度（也就是anchor）
+    f = (best * (best > thr).astype(np.float32)).mean()  # 找到每个大于阈值的标注框。求均值得到适应度
+    bpr = (best > thr).astype(np.float32).mean()  # 满足阈值的标注框所占比例
     return f, bpr
 
 
@@ -24,8 +24,8 @@ def main(img_size=512, n=9, thr=0.25, gen=1000):
 
     # 最大边缩放到img_size
     im_wh = np.array(im_wh, dtype=np.float32)
-    shapes = img_size * im_wh / im_wh.max(1, keepdims=True)
-    wh0 = np.concatenate([l * s for s, l in zip(shapes, boxes_wh)])  # wh
+    shapes = img_size * im_wh / im_wh.max(1, keepdims=True) # 输入图像都缩放到512*512，再乘相对图像大小的标注框，而不是绝对大小的标注框。这样聚类的效果会更好，而不是
+    wh0 = np.concatenate([l * s for s, l in zip(shapes, boxes_wh)])  # 绝对大小的wh。 这里的s是每张图，l是每张图里所有的标注框大小（相对），s*l就变为绝对大小
 
     # Filter 过滤掉小目标
     i = (wh0 < 3.0).any(1).sum()
@@ -36,15 +36,15 @@ def main(img_size=512, n=9, thr=0.25, gen=1000):
     # 原论文实现的是注释部分的欧式距离聚类，并在后面应用遗传算法升级了一下
     # Kmeans calculation
     # print(f'Running kmeans for {n} anchors on {len(wh)} points...')
-    # s = wh.std(0)  # sigmas for whitening
+    # s = wh.std(0)  # 原文使用的是scipy中的kmeans方法，需要除以一个标准差再传入方法
     # k, dist = kmeans(wh / s, n, iter=30)  # points, mean distance
     # assert len(k) == n, print(f'ERROR: scipy.cluster.vq.kmeans requested {n} points but returned only {len(k)}')
-    # k *= s
+    # k *= s # 得到聚类结果后再乘上标准差还原
     k = k_means(wh, n)
 
     # 按面积排序
     k = k[np.argsort(k.prod(1))]  # sort small to large
-    f, bpr = anchor_fitness(k, wh, thr)
+    f, bpr = anchor_fitness(k, wh, thr) # 计算一下求得的anchor适应度
     print("kmeans: " + " ".join([f"[{int(i[0])}, {int(i[1])}]" for i in k]))
     print(f"fitness: {f:.5f}, best possible recall: {bpr:.5f}")
 
